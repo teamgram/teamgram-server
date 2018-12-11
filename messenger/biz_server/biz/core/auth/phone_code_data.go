80a174e728932af90a6c0f66994e1796760a2a3b
benqi <wubenqi@gmail.com>
2018-12-11 14:23:40 +0800

    Filter duplicate signUp handler.

--- messenger/biz_server/biz/core/auth/phone_code_data.go
+++ messenger/biz_server/biz/core/auth/phone_code_data.go
@@ -418,7 +418,7 @@ func (code *phoneCodeData) DoSignUp(phoneCode string) error {
 	// TODO(@benqi): 重复请求处理...
 	// check state invalid.
 	// TODO(@benqi): remote client error, state is Ok
-	if do.State != kCodeStateSignIn && do.State != kCodeStateDeleted {
+	if do.State != kCodeStateSignIn && do.State != kCodeStateDeleted  && do.State != kCodeStateSignUp {
 		err := mtproto.NewRpcError(int32(mtproto.TLRpcErrorCodes_INTERNAL_SERVER_ERROR), "code state error")
 		glog.Error(err)
 		return err
