bbf1fd07728759d1509fc20ac328c5335eae96c7
wubenqi <wubenqi@gmail.com>
2019-06-19 19:56:11 +0800

    remove http_client's depends

--- pkg/http_client/httplib.go
+++ pkg/http_client/httplib.go
@@ -50,7 +50,7 @@ import (
 	"strings"
 	"sync"
 	"time"
-	"gopkg.in/yaml.v2"
+	// "gopkg.in/yaml.v2"
 )
 
 var defaultSetting = BeegoHTTPSettings{
@@ -333,19 +333,19 @@ func (b *BeegoHTTPRequest) XMLBody(obj interface{}) (*BeegoHTTPRequest, error) {
 	return b, nil
 }
 
-// YAMLBody adds request raw body encoding by YAML.
-func (b *BeegoHTTPRequest) YAMLBody(obj interface{}) (*BeegoHTTPRequest, error) {
-	if b.req.Body == nil && obj != nil {
-		byts, err := yaml.Marshal(obj)
-		if err != nil {
-			return b, err
-		}
-		b.req.Body = ioutil.NopCloser(bytes.NewReader(byts))
-		b.req.ContentLength = int64(len(byts))
-		b.req.Header.Set("Content-Type", "application/x+yaml")
-	}
-	return b, nil
-}
+//// YAMLBody adds request raw body encoding by YAML.
+//func (b *BeegoHTTPRequest) YAMLBody(obj interface{}) (*BeegoHTTPRequest, error) {
+//	if b.req.Body == nil && obj != nil {
+//		byts, err := yaml.Marshal(obj)
+//		if err != nil {
+//			return b, err
+//		}
+//		b.req.Body = ioutil.NopCloser(bytes.NewReader(byts))
+//		b.req.ContentLength = int64(len(byts))
+//		b.req.Header.Set("Content-Type", "application/x+yaml")
+//	}
+//	return b, nil
+//}
 
 // JSONBody adds request raw body encoding by JSON.
 func (b *BeegoHTTPRequest) JSONBody(obj interface{}) (*BeegoHTTPRequest, error) {
@@ -596,15 +596,15 @@ func (b *BeegoHTTPRequest) ToXML(v interface{}) error {
 	return xml.Unmarshal(data, v)
 }
 
-// ToYAML returns the map that marshals from the body bytes as yaml in response .
-// it calls Response inner.
-func (b *BeegoHTTPRequest) ToYAML(v interface{}) error {
-	data, err := b.Bytes()
-	if err != nil {
-		return err
-	}
-	return yaml.Unmarshal(data, v)
-}
+//// ToYAML returns the map that marshals from the body bytes as yaml in response .
+//// it calls Response inner.
+//func (b *BeegoHTTPRequest) ToYAML(v interface{}) error {
+//	data, err := b.Bytes()
+//	if err != nil {
+//		return err
+//	}
+//	return yaml.Unmarshal(data, v)
+//}
 
 // Response executes request client gets response mannually.
 func (b *BeegoHTTPRequest) Response() (*http.Response, error) {
