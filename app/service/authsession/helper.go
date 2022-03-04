3d12cf5bd0928ec742b8532c2f91054beb36c14c
benqi <teamgram.io@gmail.com>
2022-03-04 08:21:01 +0800

    Add service helper

---
+++ app/service/authsession/helper.go
@@ -0,0 +1,19 @@
+/*
+ * WARNING! All changes made in this file will be lost!
+ * Created from 'scheme.tl' by 'mtprotoc'
+ *
+ * Copyright 2022 Teamgram Authors.
+ *  All rights reserved.
+ *
+ * Author: teamgramio (teamgram.io@gmail.com)
+ */
+
+package authsession_helper
+
+import (
+	"github.com/teamgram/teamgram-server/app/service/authsession/internal/server"
+)
+
+var (
+	New = server.New
+)
---
+++ app/service/idgen/helper.go
@@ -0,0 +1,19 @@
+/*
+ * WARNING! All changes made in this file will be lost!
+ * Created from 'scheme.tl' by 'mtprotoc'
+ *
+ * Copyright 2022 Teamgram Authors.
+ *  All rights reserved.
+ *
+ * Author: teamgramio (teamgram.io@gmail.com)
+ */
+
+package idgen_helper
+
+import (
+	"github.com/teamgram/teamgram-server/app/service/idgen/internal/server"
+)
+
+var (
+	New = server.New
+)
