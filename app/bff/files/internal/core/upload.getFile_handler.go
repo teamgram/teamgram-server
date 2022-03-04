9c3fa7fc8c2281324e29c510b59728ecf5b9a185
benqi <teamgram.io@gmail.com>
2022-03-04 08:24:02 +0800

    Update upload.getFile_handler.go

--- app/bff/files/internal/core/upload.getFile_handler.go
+++ app/bff/files/internal/core/upload.getFile_handler.go
@@ -20,13 +20,101 @@ package core
 
 import (
 	"github.com/teamgram/proto/mtproto"
+	"github.com/teamgram/teamgram-server/app/service/dfs/dfs"
 )
 
 // UploadGetFile
 // upload.getFile#b15a9afc flags:# precise:flags.0?true cdn_supported:flags.1?true location:InputFileLocation offset:int limit:int = upload.File;
 func (c *FilesCore) UploadGetFile(in *mtproto.TLUploadGetFile) (*mtproto.Upload_File, error) {
-	// TODO: not impl
-	c.Logger.Errorf("upload.getFile blocked, License key from https://teamgram.net required to unlock enterprise features.")
+	var (
+		location = in.GetLocation()
+		offset   = in.GetOffset()
+		limit    = in.GetLimit()
+	)
 
-	return nil, mtproto.ErrEnterpriseIsBlocked
+	switch location.GetPredicateName() {
+	case mtproto.Predicate_inputFileLocation:
+		// inputFileLocation#dfdaabe1
+		//	volume_id:long
+		//	local_id:int
+		//	secret:long
+		//	file_reference:bytes = InputFileLocation;
+
+		err := mtproto.ErrInputRequestInvalid
+		c.Logger.Errorf("upload.getFile - error: %v inputFileLocation", err)
+		return nil, err
+	case mtproto.Predicate_inputEncryptedFileLocation:
+		// inputEncryptedFileLocation#f5235d55
+		//	id:long
+		//	access_hash:long = InputFileLocation;
+
+	case mtproto.Predicate_inputDocumentFileLocation:
+		// inputDocumentFileLocation#bad07584
+		//	id:long
+		//	access_hash:long
+		//	file_reference:bytes
+		//	thumb_size:string = InputFileLocation;
+		//
+	case mtproto.Predicate_inputSecureFileLocation:
+		// inputSecureFileLocation#cbc7ee28
+		//	id:long
+		//	access_hash:long = InputFileLocation;
+		//
+	case mtproto.Predicate_inputTakeoutFileLocation:
+		// inputTakeoutFileLocation#29be5899 = InputFileLocation;
+		//
+	case mtproto.Predicate_inputPhotoFileLocation:
+		// inputPhotoFileLocation#40181ffe
+		//	id:long
+		//	access_hash:long
+		//	file_reference:bytes
+		//	thumb_size:string = InputFileLocation;
+		//
+	case mtproto.Predicate_inputPeerPhotoFileLocation:
+		// inputPeerPhotoFileLocation#37257e99 flags:#
+		//	big:flags.0?true
+		//	peer:InputPeer
+		//	photo_id:long = InputFileLocation;
+		//
+	case mtproto.Predicate_inputStickerSetThumb:
+		// inputStickerSetThumb#9d84f3db
+		//	stickerset:InputStickerSet
+		//	thumb_version:int = InputFileLocation;
+		if c.svcCtx.Plugin != nil {
+			location2, err := c.svcCtx.Plugin.GetStickerSetThumbFileLocation(c.ctx,
+				c.MD.UserId,
+				location.GetStickerset(),
+				location.GetThumbVersion())
+			if err != nil {
+				c.Logger.Errorf("upload.getFile - error: %v inputFileLocation", err)
+				return nil, mtproto.ErrStickerIdInvalid
+			}
+			location = location2
+		} else {
+			c.Logger.Errorf("upload.getFile blocked, License key from https://teamgram.net required to unlock enterprise features.")
+			return nil, mtproto.ErrEnterpriseIsBlocked
+		}
+	case mtproto.Predicate_inputGroupCallStream:
+		// inputGroupCallStream#bba51639
+		//	call:InputGroupCall
+		//	time_ms:long
+		//	scale:int = InputFileLocation;
+		//
+	default:
+		c.Logger.Errorf("upload.getFile - error: invalid location")
+		return nil, mtproto.ErrLocationInvalid
+	}
+
+	uploadFile, err := c.svcCtx.Dao.DfsClient.DfsDownloadFile(c.ctx, &dfs.TLDfsDownloadFile{
+		Location: location,
+		Offset:   offset,
+		Limit:    limit,
+	})
+	if err != nil {
+		c.Logger.Errorf("upload.getFile - error: %v", err.Error())
+		// err = mtproto.ErrOffsetInvalid
+		return nil, err
+	}
+
+	return uploadFile, nil
 }
