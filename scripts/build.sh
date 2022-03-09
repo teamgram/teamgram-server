48d7dbbf57824ee515e722ab9531e6b59e973cce
benqi <teamgram.io@gmail.com>
2022-03-09 22:11:37 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,48 +1,49 @@
 #!/usr/bin/env bash
 
-teamgram="$GOPATH/src/github.com/teamgram/teamgram-server/app"
-INSTALL="$GOPATH/src/github.com/teamgram/teamgram-server/teamgramd"
+PWD=`pwd`
+TEAMGRAMAPP=${PWD}"/../app"
+INSTALL=${PWD}"/../teamgramd"
 
 echo "build idgen ..."
-cd ${teamgram}/service/idgen/cmd/idgen
+cd ${TEAMGRAMAPP}/service/idgen/cmd/idgen
 go build -o ${INSTALL}/bin/idgen
 
 echo "build status ..."
-cd ${teamgram}/service/status/cmd/status
+cd ${TEAMGRAMAPP}/service/status/cmd/status
 go build -o ${INSTALL}/bin/status
 
 echo "build dfs ..."
-cd ${teamgram}/service/dfs/cmd/dfs
+cd ${TEAMGRAMAPP}/service/dfs/cmd/dfs
 go build -o ${INSTALL}/bin/dfs
 
 echo "build media ..."
-cd ${teamgram}/service/media/cmd/media
+cd ${TEAMGRAMAPP}/service/media/cmd/media
 go build -o ${INSTALL}/bin/media
 
 echo "build authsession ..."
-cd ${teamgram}/service/authsession/cmd/authsession
+cd ${TEAMGRAMAPP}/service/authsession/cmd/authsession
 go build -o ${INSTALL}/bin/authsession
 
 echo "build biz ..."
-cd ${teamgram}/service/biz/biz/cmd/biz
+cd ${TEAMGRAMAPP}/service/biz/biz/cmd/biz
 go build -o ${INSTALL}/bin/biz
 
 echo "build msg ..."
-cd ${teamgram}/messenger/msg/cmd/msg
+cd ${TEAMGRAMAPP}/messenger/msg/cmd/msg
 go build -o ${INSTALL}/bin/msg
 
 echo "build sync ..."
-cd ${teamgram}/messenger/sync/cmd/sync
+cd ${TEAMGRAMAPP}/messenger/sync/cmd/sync
 go build -o ${INSTALL}/bin/sync
 
 echo "build bff ..."
-cd ${teamgram}/bff/bff/cmd/bff
+cd ${TEAMGRAMAPP}/bff/bff/cmd/bff
 go build -o ${INSTALL}/bin/bff
 
 echo "build session ..."
-cd ${teamgram}/interface/session/cmd/session
+cd ${TEAMGRAMAPP}/interface/session/cmd/session
 go build -o ${INSTALL}/bin/session
 
 echo "build gateway ..."
-cd ${teamgram}/interface/gateway/cmd/gateway
+cd ${TEAMGRAMAPP}/interface/gateway/cmd/gateway
 go build -o ${INSTALL}/bin/gateway
