995c6fbcb476cb9f00354643b93fc60ffdf55afc
benqi <teamgram.io@gmail.com>
2022-03-13 09:40:54 +0800

    fix user.resetNotifySettings bug

--- app/service/biz/user/internal/core/user.resetNotifySettings_handler.go
+++ app/service/biz/user/internal/core/user.resetNotifySettings_handler.go
@@ -21,7 +21,7 @@ func (c *UserCore) UserResetNotifySettings(in *user.TLUserResetNotifySettings) (
 		rValue *mtproto.Bool
 	)
 
-	if _, err := c.svcCtx.Dao.UserNotifySettingsDAO.DeleteAll(c.ctx, c.MD.UserId); err != nil {
+	if _, err := c.svcCtx.Dao.UserNotifySettingsDAO.DeleteAll(c.ctx, in.UserId); err != nil {
 		c.Logger.Errorf("user.resetNotifySettings - error: %v", err)
 		rValue = mtproto.BoolFalse
 	} else {
