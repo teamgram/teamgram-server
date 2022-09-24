6a6e5213ea11c83c3406ff1e0c1118d2b91bc599
teamgram.io <wubenqi@gmail.com>
2022-09-24 14:36:57 +0800

    Update upload.getWebFile_handler.go

--- app/bff/files/internal/core/upload.getWebFile_handler.go
+++ app/bff/files/internal/core/upload.getWebFile_handler.go
@@ -20,13 +20,25 @@ package core
 
 import (
 	"github.com/teamgram/proto/mtproto"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ErrWebfileNotAvailable = status.Error(mtproto.ErrBadRequest, "WEBFILE_NOT_AVAILABLE")
 )
 
 // UploadGetWebFile
 // upload.getWebFile#24e6818d location:InputWebFileLocation offset:int limit:int = upload.WebFile;
 func (c *FilesCore) UploadGetWebFile(in *mtproto.TLUploadGetWebFile) (*mtproto.Upload_WebFile, error) {
-	// TODO: not impl
-	c.Logger.Errorf("upload.getWebFile blocked, License key from https://teamgram.net required to unlock enterprise features.")
+	switch in.GetLocation().GetPredicateName() {
+	case mtproto.Predicate_inputWebFileAudioAlbumThumbLocation:
+		err := ErrWebfileNotAvailable
+		c.Logger.Errorf("upload.getWebFile - error: %v", err)
 
-	return nil, mtproto.ErrEnterpriseIsBlocked
+		return nil, err
+	default:
+		c.Logger.Errorf("upload.getWebFile blocked, License key from https://teamgram.net required to unlock enterprise features.")
+
+		return nil, mtproto.ErrEnterpriseIsBlocked
+	}
 }
