87dbb673bb9ab8105189a68f1a5ba194bf5fb0f8
teamgram.io <wubenqi@gmail.com>
2022-11-24 15:10:57 +0800

    fix: fix getUsers bug

--- app/bff/users/internal/core/users.getUsers_handler.go
+++ app/bff/users/internal/core/users.getUsers_handler.go
@@ -26,7 +26,8 @@ import (
 // UsersGetUsers
 // users.getUsers#d91a548 id:Vector<InputUser> = Vector<User>;
 func (c *UsersCore) UsersGetUsers(in *mtproto.TLUsersGetUsers) (*mtproto.Vector_User, error) {
-	idList := make([]int64, 0, len(in.Id))
+	idList := []int64{c.MD.UserId}
+
 	for _, inputUser := range in.Id {
 		peer := mtproto.FromInputUser(c.MD.UserId, inputUser)
 		switch peer.PeerType {
