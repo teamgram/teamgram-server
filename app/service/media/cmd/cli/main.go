8528b299e3b5bc0a3b26e8bcf8f4e64cfe24c7dc
teamgram.io <wubenqi@gmail.com>
2023-01-19 20:07:43 +0800

    feat: add media test cli

---
+++ app/service/media/cmd/cli/main.go
@@ -0,0 +1,45 @@
+// Copyright 2022 Teamgram Authors
+//  All rights reserved.
+//
+// Author: Benqi (wubenqi@gmail.com)
+//
+
+package main
+
+import (
+	"context"
+	"flag"
+
+	media_client "github.com/teamgram/teamgram-server/app/service/media/client"
+	"github.com/teamgram/teamgram-server/app/service/media/media"
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/zrpc"
+)
+
+var (
+	documentId = flag.Int64("id", 0, "the document id")
+	configFile = flag.String("f", "./cli.yaml", "the config file")
+)
+
+type Config struct {
+	MediaClient zrpc.RpcClientConf
+}
+
+func main() {
+	var (
+		c Config
+	)
+	conf.MustLoad(*configFile, &c)
+	logx.Info(c)
+
+	cli := media_client.NewMediaClient(zrpc.MustNewClient(c.MediaClient))
+	document, err := cli.MediaGetDocument(context.Background(), &media.TLMediaGetDocument{
+		Id: *documentId,
+	})
+	if err != nil {
+		logx.Errorf("getDocument - error: %v", err)
+	} else {
+		logx.Infof("getDocument - reply: %s", document.DebugString())
+	}
+}
