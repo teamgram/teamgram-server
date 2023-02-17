078eaed29e5b3fa817d7283587b9e2eec58d9765
teamgram.io <74940245+teamgramio@users.noreply.github.com>
2023-02-17 23:01:19 +0800

    feat: export user dataobject

--- app/service/biz/user/helper.go
+++ app/service/biz/user/helper.go
@@ -12,14 +12,17 @@ package user_helper
 
 import (
 	"github.com/teamgram/teamgram-server/app/service/biz/user/internal/config"
+	"github.com/teamgram/teamgram-server/app/service/biz/user/internal/dal/dataobject"
 	"github.com/teamgram/teamgram-server/app/service/biz/user/internal/dao"
 	"github.com/teamgram/teamgram-server/app/service/biz/user/internal/server/grpc/service"
 	"github.com/teamgram/teamgram-server/app/service/biz/user/internal/svc"
 )
 
 type (
-	Dao           = dao.Dao
-	CacheUserData = dao.CacheUserData
+	Dao             = dao.Dao
+	CacheUserData   = dao.CacheUserData
+	UserContactsDO  = dataobject.UserContactsDO
+	UserPresencesDO = dataobject.UserPresencesDO
 )
 
 var (
