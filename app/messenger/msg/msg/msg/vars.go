e32ad6af9471d69da419f03dd58f23d37e470e38
teamgram.io <74940245+teamgramio@users.noreply.github.com>
2023-02-21 12:27:31 +0800

    feat: add editType

---
+++ app/messenger/msg/msg/msg/vars.go
@@ -0,0 +1,13 @@
+// Copyright 2022 Teamgram Authors
+//  All rights reserved.
+//
+// Author: Benqi (wubenqi@gmail.com)
+//
+
+package msg
+
+const (
+	EditTypeNormal   = 0
+	EditTypePoll     = 1
+	EditTypeReaction = 2
+)
