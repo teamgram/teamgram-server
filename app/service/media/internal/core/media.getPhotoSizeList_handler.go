77bc7014170bc1edc6551590e3ec7857056feb49
teamgram.io <74940245+teamgramio@users.noreply.github.com>
2023-02-24 08:06:52 +0800

    feat: fix media.getPhotoSizeList bug

--- app/service/media/internal/core/media.getPhotoSizeList_handler.go
+++ app/service/media/internal/core/media.getPhotoSizeList_handler.go
@@ -17,7 +17,7 @@ import (
 // media.getPhotoSizeList size_id:long = PhotoSizeList;
 func (c *MediaCore) MediaGetPhotoSizeList(in *media.TLMediaGetPhotoSizeList) (*media.PhotoSizeList, error) {
 	cData, err := c.svcCtx.Dao.GetCachePhotoData(c.ctx, in.GetSizeId())
-	if cData != nil {
+	if err != nil {
 		c.Logger.Errorf("media.getPhotoSizeList - error: %v", err)
 		return nil, err
 	}
