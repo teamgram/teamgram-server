c17e4235982b5521035dc9afcc0b3fb1cc523504
teamgram.io <wubenqi@gmail.com>
2023-07-28 23:01:12 +0800

    feat: fix usernames_client error

--- app/bff/usernames/internal/server/grpc/service/usernames_service_impl.go
+++ app/bff/usernames/internal/server/grpc/service/usernames_service_impl.go
@@ -47,36 +47,6 @@ func (s *Service) AccountUpdateUsername(ctx context.Context, request *mtproto.TL
 	return r, err
 }
 
-// AccountReorderUsernames
-// account.reorderUsernames#ef500eab order:Vector<string> = Bool;
-func (s *Service) AccountReorderUsernames(ctx context.Context, request *mtproto.TLAccountReorderUsernames) (*mtproto.Bool, error) {
-	c := core.New(ctx, s.svcCtx)
-	c.Logger.Debugf("account.reorderUsernames - metadata: %s, request: %s", c.MD.DebugString(), request.DebugString())
-
-	r, err := c.AccountReorderUsernames(request)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Logger.Debugf("account.reorderUsernames - reply: %s", r.DebugString())
-	return r, err
-}
-
-// AccountToggleUsername
-// account.toggleUsername#58d6b376 username:string active:Bool = Bool;
-func (s *Service) AccountToggleUsername(ctx context.Context, request *mtproto.TLAccountToggleUsername) (*mtproto.Bool, error) {
-	c := core.New(ctx, s.svcCtx)
-	c.Logger.Debugf("account.toggleUsername - metadata: %s, request: %s", c.MD.DebugString(), request.DebugString())
-
-	r, err := c.AccountToggleUsername(request)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Logger.Debugf("account.toggleUsername - reply: %s", r.DebugString())
-	return r, err
-}
-
 // ContactsResolveUsername
 // contacts.resolveUsername#f93ccba3 username:string = contacts.ResolvedPeer;
 func (s *Service) ContactsResolveUsername(ctx context.Context, request *mtproto.TLContactsResolveUsername) (*mtproto.Contacts_ResolvedPeer, error) {
@@ -119,50 +89,5 @@ func (s *Service) ChannelsUpdateUsername(ctx context.Context, request *mtproto.T
 	}
 
 	c.Logger.Debugf("channels.updateUsername - reply: %s", r.DebugString())
-	return r, err
-}
-
-// ChannelsReorderUsernames
-// channels.reorderUsernames#b45ced1d channel:InputChannel order:Vector<string> = Bool;
-func (s *Service) ChannelsReorderUsernames(ctx context.Context, request *mtproto.TLChannelsReorderUsernames) (*mtproto.Bool, error) {
-	c := core.New(ctx, s.svcCtx)
-	c.Logger.Debugf("channels.reorderUsernames - metadata: %s, request: %s", c.MD.DebugString(), request.DebugString())
-
-	r, err := c.ChannelsReorderUsernames(request)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Logger.Debugf("channels.reorderUsernames - reply: %s", r.DebugString())
-	return r, err
-}
-
-// ChannelsToggleUsername
-// channels.toggleUsername#50f24105 channel:InputChannel username:string active:Bool = Bool;
-func (s *Service) ChannelsToggleUsername(ctx context.Context, request *mtproto.TLChannelsToggleUsername) (*mtproto.Bool, error) {
-	c := core.New(ctx, s.svcCtx)
-	c.Logger.Debugf("channels.toggleUsername - metadata: %s, request: %s", c.MD.DebugString(), request.DebugString())
-
-	r, err := c.ChannelsToggleUsername(request)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Logger.Debugf("channels.toggleUsername - reply: %s", r.DebugString())
-	return r, err
-}
-
-// ChannelsDeactivateAllUsernames
-// channels.deactivateAllUsernames#a245dd3 channel:InputChannel = Bool;
-func (s *Service) ChannelsDeactivateAllUsernames(ctx context.Context, request *mtproto.TLChannelsDeactivateAllUsernames) (*mtproto.Bool, error) {
-	c := core.New(ctx, s.svcCtx)
-	c.Logger.Debugf("channels.deactivateAllUsernames - metadata: %s, request: %s", c.MD.DebugString(), request.DebugString())
-
-	r, err := c.ChannelsDeactivateAllUsernames(request)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Logger.Debugf("channels.deactivateAllUsernames - reply: %s", r.DebugString())
 	return r, err
 }
