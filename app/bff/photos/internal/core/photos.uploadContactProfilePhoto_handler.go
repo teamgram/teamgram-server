f9571a2201c6e94451ef78b7455b435dcf9dc9d5
teamgram.io <wubenqi@gmail.com>
2024-02-16 10:16:30 +0800

    feat(mtproto): Update API scheme on layer 173.

--- app/bff/photos/internal/core/photos.uploadContactProfilePhoto_handler.go
+++ app/bff/photos/internal/core/photos.uploadContactProfilePhoto_handler.go
@@ -26,7 +26,7 @@ import (
 // photos.uploadContactProfilePhoto#e14c4a71 flags:# suggest:flags.3?true save:flags.4?true user_id:InputUser file:flags.0?InputFile video:flags.1?InputFile video_start_ts:flags.2?double video_emoji_markup:flags.5?VideoSize = photos.Photo;
 func (c *PhotosCore) PhotosUploadContactProfilePhoto(in *mtproto.TLPhotosUploadContactProfilePhoto) (*mtproto.Photos_Photo, error) {
 	// TODO: not impl
-	c.Logger.Errorf("photos.uploadContactProfilePhoto blocked, License key from https://teamgram.net required to unlock enterprise features.")
+	c.Logger.Errorf("photos.uploadContactProfilePhoto method not impl")
 
-	return nil, mtproto.ErrEnterpriseIsBlocked
+	return nil, mtproto.ErrMethodNotImpl
 }
