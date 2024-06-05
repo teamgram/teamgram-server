4b4aa980a58e085f7275fc14ff2f034408a12ccc
teamgram.io <wubenqi@gmail.com>
2024-06-06 07:52:30 +0800

    fix: fix bug

--- app/bff/messages/internal/core/messages.editMessage_handler.go
+++ app/bff/messages/internal/core/messages.editMessage_handler.go
@@ -69,7 +69,7 @@ func (c *MessagesCore) MessagesEditMessage(in *mtproto.TLMessagesEditMessage) (*
 
 	// TODO(@benqi): check
 	// editMessage := editMessages[0]
-	if editMessages.Datas[0].UserId != c.MD.UserId {
+	if editMessages.Datas[0].SenderUserId != c.MD.UserId {
 		err = mtproto.ErrMessageAuthorRequired
 		c.Logger.Errorf("messages.editMessage - emptyMessage(%d)", in.Id)
 		return nil, err
