9c0539d07cf3c456ca01412c185e562d983ff8c0
teamgram.io <wubenqi@gmail.com>
2024-10-14 16:40:18 +0800

    feat: disable messages.getOutboxReadDate

--- app/bff/messages/internal/core/messages.getOutboxReadDate_handler.go
+++ app/bff/messages/internal/core/messages.getOutboxReadDate_handler.go
@@ -26,7 +26,8 @@ import (
 // messages.getOutboxReadDate#8c4bfe5d peer:InputPeer msg_id:int = OutboxReadDate;
 func (c *MessagesCore) MessagesGetOutboxReadDate(in *mtproto.TLMessagesGetOutboxReadDate) (*mtproto.OutboxReadDate, error) {
 	// TODO: not impl
-	c.Logger.Errorf("messages.getOutboxReadDate blocked, License key from https://teamgram.net required to unlock enterprise features.")
+	// c.Logger.Errorf("messages.getOutboxReadDate blocked, License key from https://teamgram.net required to unlock enterprise features.")
 
-	return nil, mtproto.ErrEnterpriseIsBlocked
+	//
+	return nil, mtproto.ErrUserPrivacyRestricted
 }
