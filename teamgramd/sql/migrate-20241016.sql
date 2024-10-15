05fdac6d79943bb097360ffd12a1ebbafea0704a
teamgram.io <wubenqi@gmail.com>
2024-10-16 00:33:04 +0800

    feat: messages.getOutboxReadDate

--- teamgramd/sql/migrate-20241016.sql
+++ teamgramd/sql/migrate-20241016.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： 127.0.0.1
--- 生成日期： 2024-10-15 11:49:12
+-- 生成日期： 2024-10-15 16:31:04
 -- 服务器版本： 8.0.28
 -- PHP 版本： 8.3.11
 
@@ -30,16 +30,14 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `message_read_outbox` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
-  `message_id` int NOT NULL,
-  `peer_type` int NOT NULL,
-  `peer_id` int NOT NULL,
+  `peer_dialog_id` bigint NOT NULL,
   `read_user_id` bigint NOT NULL,
-  `read_date` bigint NOT NULL,
+  `read_outbox_max_id` int NOT NULL,
+  `read_outbox_max_date` bigint NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`user_id`,`message_id`,`read_user_id`),
-  KEY `user_id_2` (`user_id`,`message_id`)
+  UNIQUE KEY `user_id` (`user_id`,`peer_dialog_id`,`read_user_id`,`read_outbox_max_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 COMMIT;
 
