ac459df24d821409102105399f4b5657e826e3be
teamgram.io <wubenqi@gmail.com>
2024-11-23 17:55:23 +0800

    feat: enable account.setAuthorizationTTL

--- teamgramd/sql/migrate-20241123.sql
+++ teamgramd/sql/migrate-20241123.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `auth_users` DROP `layer`, DROP `device_model`, DROP `platform`, DROP `system_version`, DROP `api_id`, DROP `app_name`, DROP `app_version`, DROP `ip`, DROP `country`, DROP `region`;
 ALTER TABLE `auth_users` CHANGE `date_actived` `date_active` BIGINT NOT NULL DEFAULT '0';
-ALTER TABLE `auth_users` ADD `authorization_ttl_days` INT NOT NULL DEFAULT '180' AFTER `android_push_session_id`;
