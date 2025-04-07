0e617d66f139bb496f8ae571328ead6f75c64d27
wubenqi <wubenqi@gmail.com>
2025-04-07 15:41:59 +0800

    feat: updates

--- pkg/env2/env2.go
+++ pkg/env2/env2.go
@@ -11,13 +11,13 @@ import (
 )
 
 var (
-	MyAppName       = "Teamgram"
-	MyWebSite       = "teamgram.net"
-	TDotMe          = "t.me"
-	MyWebClientSite = "web.teamgram.net"
-	MyTgScheme      = "tg"
-	IosAppStoreId   = ""
-	AndroidPackage  = "org.chatengine.messenger"
+	MyAppName = "Teamgram"
+	MyWebSite = "teamgram.net"
+	TDotMe    = "t.me"
+	// MyWebClientSite = "web.teamgram.net"
+	MyTgScheme = "tg"
+	// IosAppStoreId   = ""
+	// AndroidPackage  = "org.chatengine.messenger"
 
 	// PredefinedUser - auto register
 	PredefinedUser = false
@@ -30,10 +30,10 @@ func init() {
 	flag.StringVar(&MyAppName, "app_name", "Teamgram", "app_name")
 	flag.StringVar(&MyWebSite, "site_name", "teamgram.net", "site_name")
 	flag.StringVar(&TDotMe, "t.me", "t.me", "t.me")
-	flag.StringVar(&MyWebClientSite, "webclient", "web.teamgram.net", "web.teamgram.net")
+	// flag.StringVar(&MyWebClientSite, "webclient", "web.teamgram.net", "web.teamgram.net")
 	flag.StringVar(&MyTgScheme, "tg", "tg", "tg")
-	flag.StringVar(&IosAppStoreId, "app_store_id", "", "")
-	flag.StringVar(&AndroidPackage, "android_package", "org.chatengine.messenger", "org.chatengine.messenger")
+	// flag.StringVar(&IosAppStoreId, "app_store_id", "", "")
+	// flag.StringVar(&AndroidPackage, "android_package", "org.chatengine.messenger", "org.chatengine.messenger")
 	flag.BoolVar(&PredefinedUser, "predefined", false, "predefined")
 	flag.BoolVar(&PredefinedUser2, "predefined2", false, "predefined2")
 }
