5666e15073892a5b03cf32097d26a31fe534ed55
wubenqi <wubenqi@gmail.com>
2025-05-30 19:15:00 +0800

    feat: enable setChatWallpaper

--- app/service/biz/dialog/internal/core/dialog.setChatWallpaper_handler.go
+++ app/service/biz/dialog/internal/core/dialog.setChatWallpaper_handler.go
@@ -19,6 +19,9 @@
 package core
 
 import (
+	"context"
+	
+	"github.com/teamgram/marmota/pkg/stores/sqlx"
 	"github.com/teamgram/proto/mtproto"
 	"github.com/teamgram/teamgram-server/app/service/biz/dialog/dialog"
 )
@@ -26,27 +29,38 @@ import (
 // DialogSetChatWallpaper
 // dialog.setChatWallpaper flags:# user_id:long peer_type:int peer_id:long wallpaper_id:long wallpaper_overridden:flags.0?true = Bool;
 func (c *DialogCore) DialogSetChatWallpaper(in *dialog.TLDialogSetChatWallpaper) (*mtproto.Bool, error) {
-	if in.WallpaperId != 0 {
-		_, _ = c.svcCtx.Dao.DialogsDAO.UpdateCustomMap(
-			c.ctx,
-			map[string]interface{}{
-				"wallpaper_id":         in.WallpaperId,
-				"wallpaper_overridden": in.WallpaperOverridden,
-			},
-			in.UserId,
-			in.PeerType,
-			in.PeerId)
-	} else {
-		_, _ = c.svcCtx.Dao.DialogsDAO.UpdateCustomMap(
-			c.ctx,
-			map[string]interface{}{
-				"wallpaper_id":         0,
-				"wallpaper_overridden": false,
-			},
-			in.UserId,
-			in.PeerType,
-			in.PeerId)
-	}
+	c.svcCtx.Dao.CachedConn.Exec(
+		c.ctx,
+		func(ctx context.Context, conn *sqlx.DB) (int64, int64, error) {
+			var (
+				rowsAffected int64
+			)
+
+			if in.WallpaperId != 0 {
+				rowsAffected, _ = c.svcCtx.Dao.DialogsDAO.UpdateCustomMap(
+					c.ctx,
+					map[string]interface{}{
+						"wallpaper_id":         in.WallpaperId,
+						"wallpaper_overridden": in.WallpaperOverridden,
+					},
+					in.UserId,
+					in.PeerType,
+					in.PeerId)
+			} else {
+				rowsAffected, _ = c.svcCtx.Dao.DialogsDAO.UpdateCustomMap(
+					c.ctx,
+					map[string]interface{}{
+						"wallpaper_id":         0,
+						"wallpaper_overridden": false,
+					},
+					in.UserId,
+					in.PeerType,
+					in.PeerId)
+			}
+
+			return 0, rowsAffected, nil
+		},
+		dialog.GetDialogCacheKey(in.UserId, mtproto.MakePeerDialogId(in.PeerType, in.PeerId)))
 
 	return mtproto.BoolTrue, nil
 }
