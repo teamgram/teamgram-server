1b8942ef3de1920b0588aa2bd1885e77420f3f0d
wubenqi <wubenqi@gmail.com>
2026-02-03 14:21:37 +0800

    Initial commit

--- pkg/net/kitex/codec/codec.go
+++ pkg/net/kitex/codec/codec.go
@@ -20,9 +20,9 @@ import (
 	"context"
 	"encoding/binary"
 	"encoding/hex"
-	"encoding/json"
 	"errors"
 	"fmt"
+
 	"github.com/teamgooo/teamgooo-server/pkg/proto/bin"
 	"github.com/teamgooo/teamgooo-server/pkg/proto/iface"
 	"github.com/teamgooo/teamgooo-server/pkg/proto/iface/ecode"
@@ -33,6 +33,11 @@ import (
 	"github.com/cloudwego/kitex/pkg/remote"
 	"github.com/cloudwego/kitex/pkg/remote/codec"
 	"github.com/cloudwego/kitex/pkg/remote/codec/perrors"
+)
+
+const (
+	// Magic number for ZRPC protocol (ZRPC in ASCII: 0x5A525043)
+	zrpcMagicNumber = uint32(0x5A525043)
 )
 
 type ZRpcCodec struct {
@@ -49,10 +54,8 @@ func (jc *ZRpcCodec) Encode(ctx context.Context, message remote.Message, out rem
 	case remote.Exception:
 		switch e := message.Data().(type) {
 		case *remote.TransError:
-			// validData = &Exception{e.TypeID(), e.Error()}
 			validData = tg.NewRpcError(e)
 		case error:
-			// validData = &Exception{remote.InternalError, e.Error()}
 			validData = tg.NewRpcError(e)
 		default:
 			return errors.New("exception relay must implement error type")
@@ -63,17 +66,16 @@ func (jc *ZRpcCodec) Encode(ctx context.Context, message remote.Message, out rem
 
 	klog.Infof("trans: %v", message.TransInfo().TransStrInfo())
 
+	// Encode payload using TL binary protocol
 	x := bin.NewEncoder()
 	defer x.End()
 	_ = validData.(iface.TLObject).Encode(x, 201)
 	payload := x.Bytes()
-	//payload, err := json.Marshal(validData)
-	//if err != nil {
-	//	return perrors.NewProtocolError(fmt.Errorf("json encode, marshal payload failed: %w", err))
-	//}
+
 	if jc.printDebugInfo {
 		klog.Infof("encoded payload: %s\n", hex.EncodeToString(payload))
 	}
+
 	data := &Meta{
 		ServiceName: message.RPCInfo().Invocation().ServiceName(),
 		MethodName:  message.RPCInfo().Invocation().MethodName(),
@@ -82,40 +84,71 @@ func (jc *ZRpcCodec) Encode(ctx context.Context, message remote.Message, out rem
 		Payload:     payload,
 		Metadata:    message.TransInfo().TransStrInfo(),
 	}
-	buf, err := json.Marshal(data)
+
+	// Encode Meta using binary format
+	buf, err := jc.encodeMeta(data)
 	if err != nil {
-		return perrors.NewProtocolError(fmt.Errorf("json encode, marshal data failed: %w", err))
+		return perrors.NewProtocolError(fmt.Errorf("binary encode, marshal data failed: %w", err))
 	}
-	length := make([]byte, 8)
-	binary.BigEndian.PutUint64(length, uint64(len(buf)))
+
+	// Write magic number (4 bytes)
+	magic := make([]byte, 4)
+	binary.BigEndian.PutUint32(magic, zrpcMagicNumber)
+	_, err = out.WriteBinary(magic)
+	if err != nil {
+		return perrors.NewProtocolError(fmt.Errorf("binary encode, write magic failed: %w", err))
+	}
+
+	// Write total length (4 bytes)
+	length := make([]byte, 4)
+	binary.BigEndian.PutUint32(length, uint32(len(buf)))
 	_, err = out.WriteBinary(length)
 	if err != nil {
-		return perrors.NewProtocolError(fmt.Errorf("json encode, write length failed: %w", err))
+		return perrors.NewProtocolError(fmt.Errorf("binary encode, write length failed: %w", err))
 	}
+
+	// Write data
 	_, err = out.WriteBinary(buf)
 	if err != nil {
-		return perrors.NewProtocolError(fmt.Errorf("json encode, write data failed: %w", err))
+		return perrors.NewProtocolError(fmt.Errorf("binary encode, write data failed: %w", err))
 	}
+
 	return nil
 }
 
 func (jc *ZRpcCodec) Decode(ctx context.Context, message remote.Message, in remote.ByteBuffer) error {
-	length := dirtmake.Bytes(8, 8)
-	_, err := in.ReadBinary(length)
+	// Read magic number (4 bytes)
+	magic := dirtmake.Bytes(4, 4)
+	_, err := in.ReadBinary(magic)
 	if err != nil {
-		return perrors.NewProtocolError(fmt.Errorf("json decode, read length failed: %w", err))
+		return perrors.NewProtocolError(fmt.Errorf("binary decode, read magic failed: %w", err))
 	}
-	l := int(binary.BigEndian.Uint64(length))
+	magicNum := binary.BigEndian.Uint32(magic)
+	if magicNum != zrpcMagicNumber {
+		return perrors.NewProtocolError(fmt.Errorf("binary decode, invalid magic number: 0x%08X, expected: 0x%08X", magicNum, zrpcMagicNumber))
+	}
+
+	// Read length (4 bytes)
+	length := dirtmake.Bytes(4, 4)
+	_, err = in.ReadBinary(length)
+	if err != nil {
+		return perrors.NewProtocolError(fmt.Errorf("binary decode, read length failed: %w", err))
+	}
+	l := int(binary.BigEndian.Uint32(length))
+
+	// Read data
 	buf := dirtmake.Bytes(l, l)
 	_, err = in.ReadBinary(buf)
 	if err != nil {
-		return perrors.NewProtocolError(fmt.Errorf("json decode, read data failed: %w", err))
+		return perrors.NewProtocolError(fmt.Errorf("binary decode, read data failed: %w", err))
 	}
-	data := &Meta{}
-	err = json.Unmarshal(buf, data)
+
+	// Decode Meta from binary format
+	data, err := jc.decodeMeta(buf)
 	if err != nil {
-		return perrors.NewProtocolError(fmt.Errorf("json decode, unmarshal Meta data failed: %w", err))
+		return perrors.NewProtocolError(fmt.Errorf("binary decode, unmarshal Meta data failed: %w", err))
 	}
+
 	if err = codec.SetOrCheckSeqID(data.SeqID, message); err != nil {
 		return err
 	}
@@ -129,29 +162,23 @@ func (jc *ZRpcCodec) Decode(ctx context.Context, message remote.Message, in remo
 	if jc.printDebugInfo {
 		klog.Infof("encoded payload: %s\n", hex.EncodeToString(data.Payload))
 	}
+
 	if remote.MessageType(data.MsgType) == remote.Exception {
-		//var exception tg.TLRpcError
-		//d := bin.NewDecoder(data.Payload)
-		//err = exception.Decode(d)
 		exception2, err2 := tg.DecodeRpcErrorClazz(bin.NewDecoder(data.Payload))
-		// var exception Exception
-		// err = json.Unmarshal(data.Payload, &exception)
 		if err2 != nil {
-			return perrors.NewProtocolError(fmt.Errorf("json decode, unmarshal Exception payload failed: %w", err2))
+			return perrors.NewProtocolError(fmt.Errorf("binary decode, unmarshal Exception payload failed: %w", err2))
 		} else if exception, ok := exception2.(*tg.TLRpcError); !ok {
-			return perrors.NewProtocolError(fmt.Errorf("json decode, unmarshal Exception payload failed: %w", exception2))
+			return perrors.NewProtocolError(fmt.Errorf("binary decode, invalid Exception type: %T", exception2))
 		} else {
 			return ecode.NewCodeError(exception.Code(), exception.ErrorMessage)
 		}
 	}
 
+	// Decode payload using TL binary protocol
 	d := bin.NewDecoder(data.Payload)
-	// data2, err = iface.DecodeObject(d)
 	err = message.Data().(iface.TLObject).Decode(d)
-
-	//err = json.Unmarshal(data.Payload, message.Data())
 	if err != nil {
-		return perrors.NewProtocolError(fmt.Errorf("json decode, unmarshal payload failed: %w", err))
+		return perrors.NewProtocolError(fmt.Errorf("binary decode, unmarshal payload failed: %w", err))
 	}
 
 	klog.Infof("trans: %v", data.Metadata)
@@ -163,4 +190,165 @@ func (jc *ZRpcCodec) Decode(ctx context.Context, message remote.Message, in remo
 
 func (jc *ZRpcCodec) Name() string {
 	return "ZRPC"
+}
+
+// encodeMeta encodes Meta struct into binary format
+func (jc *ZRpcCodec) encodeMeta(meta *Meta) ([]byte, error) {
+	// Calculate buffer size
+	size := 0
+	size += 2 + len(meta.ServiceName) // ServiceName length (2 bytes) + data
+	size += 2 + len(meta.MethodName)  // MethodName length (2 bytes) + data
+	size += 4                         // SeqID (4 bytes)
+	size += 4                         // MsgType (4 bytes)
+	size += 4 + len(meta.Payload)     // Payload length (4 bytes) + data
+	size += 2                         // Metadata count (2 bytes)
+	for k, v := range meta.Metadata {
+		size += 2 + len(k) // key length + key
+		size += 2 + len(v) // value length + value
+	}
+
+	buf := make([]byte, size)
+	offset := 0
+
+	// Write ServiceName
+	binary.BigEndian.PutUint16(buf[offset:], uint16(len(meta.ServiceName)))
+	offset += 2
+	copy(buf[offset:], meta.ServiceName)
+	offset += len(meta.ServiceName)
+
+	// Write MethodName
+	binary.BigEndian.PutUint16(buf[offset:], uint16(len(meta.MethodName)))
+	offset += 2
+	copy(buf[offset:], meta.MethodName)
+	offset += len(meta.MethodName)
+
+	// Write SeqID
+	binary.BigEndian.PutUint32(buf[offset:], uint32(meta.SeqID))
+	offset += 4
+
+	// Write MsgType
+	binary.BigEndian.PutUint32(buf[offset:], meta.MsgType)
+	offset += 4
+
+	// Write Payload
+	binary.BigEndian.PutUint32(buf[offset:], uint32(len(meta.Payload)))
+	offset += 4
+	copy(buf[offset:], meta.Payload)
+	offset += len(meta.Payload)
+
+	// Write Metadata
+	binary.BigEndian.PutUint16(buf[offset:], uint16(len(meta.Metadata)))
+	offset += 2
+	for k, v := range meta.Metadata {
+		// Write key
+		binary.BigEndian.PutUint16(buf[offset:], uint16(len(k)))
+		offset += 2
+		copy(buf[offset:], k)
+		offset += len(k)
+		// Write value
+		binary.BigEndian.PutUint16(buf[offset:], uint16(len(v)))
+		offset += 2
+		copy(buf[offset:], v)
+		offset += len(v)
+	}
+
+	return buf, nil
+}
+
+// decodeMeta decodes Meta struct from binary format
+func (jc *ZRpcCodec) decodeMeta(buf []byte) (*Meta, error) {
+	if len(buf) < 4 {
+		return nil, fmt.Errorf("buffer too small")
+	}
+
+	meta := &Meta{}
+	offset := 0
+
+	// Read ServiceName
+	if offset+2 > len(buf) {
+		return nil, fmt.Errorf("invalid ServiceName length")
+	}
+	serviceNameLen := int(binary.BigEndian.Uint16(buf[offset:]))
+	offset += 2
+	if offset+serviceNameLen > len(buf) {
+		return nil, fmt.Errorf("invalid ServiceName data")
+	}
+	meta.ServiceName = string(buf[offset : offset+serviceNameLen])
+	offset += serviceNameLen
+
+	// Read MethodName
+	if offset+2 > len(buf) {
+		return nil, fmt.Errorf("invalid MethodName length")
+	}
+	methodNameLen := int(binary.BigEndian.Uint16(buf[offset:]))
+	offset += 2
+	if offset+methodNameLen > len(buf) {
+		return nil, fmt.Errorf("invalid MethodName data")
+	}
+	meta.MethodName = string(buf[offset : offset+methodNameLen])
+	offset += methodNameLen
+
+	// Read SeqID
+	if offset+4 > len(buf) {
+		return nil, fmt.Errorf("invalid SeqID")
+	}
+	meta.SeqID = int32(binary.BigEndian.Uint32(buf[offset:]))
+	offset += 4
+
+	// Read MsgType
+	if offset+4 > len(buf) {
+		return nil, fmt.Errorf("invalid MsgType")
+	}
+	meta.MsgType = binary.BigEndian.Uint32(buf[offset:])
+	offset += 4
+
+	// Read Payload
+	if offset+4 > len(buf) {
+		return nil, fmt.Errorf("invalid Payload length")
+	}
+	payloadLen := int(binary.BigEndian.Uint32(buf[offset:]))
+	offset += 4
+	if offset+payloadLen > len(buf) {
+		return nil, fmt.Errorf("invalid Payload data")
+	}
+	meta.Payload = make([]byte, payloadLen)
+	copy(meta.Payload, buf[offset:offset+payloadLen])
+	offset += payloadLen
+
+	// Read Metadata
+	if offset+2 > len(buf) {
+		return nil, fmt.Errorf("invalid Metadata count")
+	}
+	metadataCount := int(binary.BigEndian.Uint16(buf[offset:]))
+	offset += 2
+	meta.Metadata = make(map[string]string, metadataCount)
+	for i := 0; i < metadataCount; i++ {
+		// Read key
+		if offset+2 > len(buf) {
+			return nil, fmt.Errorf("invalid Metadata key length")
+		}
+		keyLen := int(binary.BigEndian.Uint16(buf[offset:]))
+		offset += 2
+		if offset+keyLen > len(buf) {
+			return nil, fmt.Errorf("invalid Metadata key data")
+		}
+		key := string(buf[offset : offset+keyLen])
+		offset += keyLen
+
+		// Read value
+		if offset+2 > len(buf) {
+			return nil, fmt.Errorf("invalid Metadata value length")
+		}
+		valueLen := int(binary.BigEndian.Uint16(buf[offset:]))
+		offset += 2
+		if offset+valueLen > len(buf) {
+			return nil, fmt.Errorf("invalid Metadata value data")
+		}
+		value := string(buf[offset : offset+valueLen])
+		offset += valueLen
+
+		meta.Metadata[key] = value
+	}
+
+	return meta, nil
 }
---
+++ pkg/net/kitex/codec/codec_test.go
@@ -0,0 +1,188 @@
+// Copyright (c) 2024 The Teamgram Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: teamgramio (teamgram.io@gmail.com)
+
+package codec
+
+import (
+	"encoding/binary"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncodeMeta(t *testing.T) {
+	codec := &ZRpcCodec{printDebugInfo: false}
+
+	meta := &Meta{
+		ServiceName: "test.service",
+		MethodName:  "TestMethod",
+		SeqID:       12345,
+		MsgType:     1,
+		Payload:     []byte{0x01, 0x02, 0x03, 0x04},
+		Metadata: map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+
+	// Encode
+	buf, err := codec.encodeMeta(meta)
+	assert.NoError(t, err)
+	assert.NotNil(t, buf)
+
+	// Verify magic number is NOT in encoded meta (it's added in Encode, not encodeMeta)
+	// Verify the buffer structure manually
+	offset := 0
+
+	// ServiceName length
+	serviceNameLen := binary.BigEndian.Uint16(buf[offset:])
+	assert.Equal(t, uint16(len(meta.ServiceName)), serviceNameLen)
+	offset += 2
+
+	// ServiceName
+	serviceName := string(buf[offset : offset+int(serviceNameLen)])
+	assert.Equal(t, meta.ServiceName, serviceName)
+	offset += int(serviceNameLen)
+
+	// MethodName length
+	methodNameLen := binary.BigEndian.Uint16(buf[offset:])
+	assert.Equal(t, uint16(len(meta.MethodName)), methodNameLen)
+	offset += 2
+
+	// MethodName
+	methodName := string(buf[offset : offset+int(methodNameLen)])
+	assert.Equal(t, meta.MethodName, methodName)
+	offset += int(methodNameLen)
+
+	// SeqID
+	seqID := int32(binary.BigEndian.Uint32(buf[offset:]))
+	assert.Equal(t, meta.SeqID, seqID)
+	offset += 4
+
+	// MsgType
+	msgType := binary.BigEndian.Uint32(buf[offset:])
+	assert.Equal(t, meta.MsgType, msgType)
+	offset += 4
+
+	// Payload length
+	payloadLen := binary.BigEndian.Uint32(buf[offset:])
+	assert.Equal(t, uint32(len(meta.Payload)), payloadLen)
+	offset += 4
+
+	// Payload
+	payload := buf[offset : offset+int(payloadLen)]
+	assert.Equal(t, meta.Payload, payload)
+	offset += int(payloadLen)
+
+	// Metadata count
+	metadataCount := binary.BigEndian.Uint16(buf[offset:])
+	assert.Equal(t, uint16(len(meta.Metadata)), metadataCount)
+}
+
+func TestDecodeEncodeMeta(t *testing.T) {
+	codec := &ZRpcCodec{printDebugInfo: false}
+
+	original := &Meta{
+		ServiceName: "test.service",
+		MethodName:  "TestMethod",
+		SeqID:       12345,
+		MsgType:     1,
+		Payload:     []byte{0x01, 0x02, 0x03, 0x04},
+		Metadata: map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+
+	// Encode
+	buf, err := codec.encodeMeta(original)
+	assert.NoError(t, err)
+
+	// Decode
+	decoded, err := codec.decodeMeta(buf)
+	assert.NoError(t, err)
+	assert.NotNil(t, decoded)
+
+	// Verify
+	assert.Equal(t, original.ServiceName, decoded.ServiceName)
+	assert.Equal(t, original.MethodName, decoded.MethodName)
+	assert.Equal(t, original.SeqID, decoded.SeqID)
+	assert.Equal(t, original.MsgType, decoded.MsgType)
+	assert.Equal(t, original.Payload, decoded.Payload)
+	assert.Equal(t, len(original.Metadata), len(decoded.Metadata))
+	for k, v := range original.Metadata {
+		assert.Equal(t, v, decoded.Metadata[k])
+	}
+}
+
+func TestMagicNumber(t *testing.T) {
+	// Verify magic number is correct
+	assert.Equal(t, uint32(0x5A525043), zrpcMagicNumber)
+
+	// Verify it spells "ZRPC" in ASCII
+	magicBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(magicBytes, zrpcMagicNumber)
+	assert.Equal(t, byte('Z'), magicBytes[0])
+	assert.Equal(t, byte('R'), magicBytes[1])
+	assert.Equal(t, byte('P'), magicBytes[2])
+	assert.Equal(t, byte('C'), magicBytes[3])
+}
+
+func BenchmarkEncodeMeta(b *testing.B) {
+	codec := &ZRpcCodec{printDebugInfo: false}
+
+	meta := &Meta{
+		ServiceName: "test.service",
+		MethodName:  "TestMethod",
+		SeqID:       12345,
+		MsgType:     1,
+		Payload:     make([]byte, 1024), // 1KB payload
+		Metadata: map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+			"key3": "value3",
+		},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = codec.encodeMeta(meta)
+	}
+}
+
+func BenchmarkDecodeMeta(b *testing.B) {
+	codec := &ZRpcCodec{printDebugInfo: false}
+
+	meta := &Meta{
+		ServiceName: "test.service",
+		MethodName:  "TestMethod",
+		SeqID:       12345,
+		MsgType:     1,
+		Payload:     make([]byte, 1024), // 1KB payload
+		Metadata: map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+			"key3": "value3",
+		},
+	}
+
+	buf, _ := codec.encodeMeta(meta)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = codec.decodeMeta(buf)
+	}
+}
