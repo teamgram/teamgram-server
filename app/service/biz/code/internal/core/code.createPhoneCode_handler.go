46a463f8c546d26a3c3311c0967626d3e379160b
wubenqi <wubenqi@gmail.com>
2026-04-27 12:21:51 +0800

    feat(code): implement createPhoneCode handler

--- app/service/biz/code/internal/core/code.createPhoneCode_handler.go
+++ app/service/biz/code/internal/core/code.createPhoneCode_handler.go
@@ -17,15 +17,61 @@
 package core
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"math/big"
+	"time"
+
 	"github.com/teamgram/teamgram-server/v2/app/service/biz/code/code"
-	"github.com/teamgram/teamgram-server/v2/pkg/proto/tg"
 )
+
+const (
+	codeStateSend      = 1
+	codeLen            = 5
+	codeHashLen        = 16
+	codeExpireDuration = 3 * 60 // seconds
+)
+
+func randomNumeric(n int) string {
+	const digits = "0123456789"
+	b := make([]byte, n)
+	for i := range b {
+		idx, _ := rand.Int(rand.Reader, big.NewInt(int64(len(digits))))
+		b[i] = digits[idx.Int64()]
+	}
+	return string(b)
+}
+
+func randomHexString(n int) string {
+	b := make([]byte, n)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b)
+}
 
 // CodeCreatePhoneCode
 // code.createPhoneCode flags:# auth_key_id:long session_id:long phone:string phone_number_registered:flags.0?true sent_code_type:int next_code_type:int state:int = PhoneCodeTransaction;
 func (c *CodeCore) CodeCreatePhoneCode(in *code.TLCodeCreatePhoneCode) (*code.PhoneCodeTransaction, error) {
-	// TODO: not impl
-	c.Logger.Errorf("code.createPhoneCode - error: method CodeCreatePhoneCode not impl")
+	codeData, err := c.repo.GetCachePhoneCode(c.ctx, in.AuthKeyId, in.Phone)
+	if err != nil {
+		c.Logger.Errorf("code.createPhoneCode - get cache failed: auth_key_id: %d, phone: %s, err: %v",
+			in.AuthKeyId, in.Phone, err)
+	}
 
-	return nil, tg.ErrMethodNotImpl
+	if codeData == nil || in.SessionId != codeData.SessionId {
+		codeData = code.MakeTLPhoneCodeTransaction(&code.TLPhoneCodeTransaction{
+			AuthKeyId:             in.AuthKeyId,
+			SessionId:             in.SessionId,
+			Phone:                 in.Phone,
+			PhoneNumberRegistered: in.PhoneNumberRegistered,
+			PhoneCode:             randomNumeric(codeLen),
+			PhoneCodeHash:         randomHexString(codeHashLen),
+			PhoneCodeExpired:      int32(time.Now().Unix() + codeExpireDuration),
+			SentCodeType:          in.SentCodeType,
+			FlashCallPattern:      "*",
+			NextCodeType:          in.NextCodeType,
+			State:                 codeStateSend,
+		})
+	}
+
+	return codeData, nil
 }
