9e9073874637181007f7b6327c7ac55f9de7c07d
wubenqi <wubenqi@gmail.com>
2026-05-03 21:34:16 +0800

    feat(account): implement authorization reset and deletion

--- app/bff/account/internal/core/account.resetAuthorization_handler.go
+++ app/bff/account/internal/core/account.resetAuthorization_handler.go
@@ -17,14 +17,32 @@
 package core
 
 import (
+	"github.com/teamgram/teamgram-server/v2/app/service/authsession/authsession"
 	"github.com/teamgram/teamgram-server/v2/pkg/proto/tg"
 )
 
 // AccountResetAuthorization
 // account.resetAuthorization#df77f3bc hash:long = Bool;
 func (c *AccountCore) AccountResetAuthorization(in *tg.TLAccountResetAuthorization) (*tg.Bool, error) {
-	// TODO: not impl
-	c.Logger.Errorf("account.resetAuthorization - error: method AccountResetAuthorization not impl")
+	selfID, err := requireSelfID(c)
+	if err != nil {
+		return nil, err
+	}
+	if in == nil || in.Hash == 0 {
+		return tg.BoolFalse, nil
+	}
+	if err := requireAuthsessionClient(c); err != nil {
+		return nil, err
+	}
 
-	return nil, tg.ErrMethodNotImpl
+	if _, err = c.svcCtx.Repo.AuthsessionClient.AuthsessionResetAuthorization(c.ctx, &authsession.TLAuthsessionResetAuthorization{
+		UserId:    selfID,
+		AuthKeyId: accountAuthKeyID(c),
+		Hash:      in.Hash,
+	}); err != nil {
+		return nil, err
+	}
+
+	// TODO(v2 account): master sent updatesTooLong and updateAccountResetAuthorization through sync; do not migrate sync calls until v2 userupdates/gateway delivery is defined.
+	return tg.BoolTrue, nil
 }
